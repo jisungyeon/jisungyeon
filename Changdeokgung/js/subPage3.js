a420a3c795be365e80fa3f74408fc8dc9f9f8cbd
jisungyeon <oracle1120@naver.com>
2023-08-09 18:12:18 +0900

    창덕궁 추가됨

---
+++ Changdeokgung/js/subPage3.js
@@ -0,0 +1,45 @@
+const slider = $("#slider");
+
+slider.slick({
+    arrows: true,
+    prevArrow: $(".dateSlider").find(".prevArrow"),
+    nextArrow: $(".dateSlider").find(".nextArrow"),
+});
+
+
+
+// ---------날짜 선택-------------
+
+$("nav .available").on("click", function () {
+    // $(this).addClass("on");
+    // $("nav .available").not(this).removeClass("on");
+
+    if ($(this).hasClass("on")) {
+        $(this).removeClass("on");
+    } else  {
+        $(this).addClass("on");
+    }
+
+    $('nav .available').not(this).removeClass('on');
+});
+
+
+// ---------시간 선택-------------
+
+$(".timeWrap nav .sell").on("click", function() {
+    $(this).toggleClass("on");
+    
+    $(".timeWrap nav .sell").not(this).removeClass("on");
+    $(".timeWrap nav .soldOut").removeClass("off");
+});
+
+$(".timeWrap nav .soldOut").on("click", function() {
+    $(this).toggleClass("off");
+
+    $(".timeWrap nav .soldOut").not(this).removeClass("off");
+    $(".timeWrap nav .sell").removeClass("on");
+});
+  
+
+
+
